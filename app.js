de814de8c86596c789ff4b7ce262aaf526611417
EnterOrion <orionwesson@gmail.com>
2022-12-17 16:00:31 +0100

    Add functionality for first player

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const gameBoard = (function() {
-    let _board = ["x", "x", "x", "o", "o", "x", "o", "x", "x"];
+    let _board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     let boardSquares = document.getElementsByClassName("square");
+    for(let i=0; i<boardSquares.length; i++) {
+        boardSquares[i].addEventListener("click", () => {
+            console.log("success!");
+            _board[i] = gameController.currentPlayer.marker;
+            convert();
+        })
+    }
+
     let convert = () => {
         for(let i=0; i<boardSquares.length; i++) {
             boardSquares[i].innerHTML = _board[i];
           }
     }
-    convert();
 })();
 
 const Player = (name, marker) => {
     return {name, marker}
 }
+
+const gameController = (() => {
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+
+    let currentPlayer = player1;
+    if (currentPlayer == player1) {
+        document.getElementById("player1").style.backgroundColor = "rgb(153, 153, 211, 0.5)";
+    }
+    else {
+        document.getElementById("player2").style.backgroundColor = "rgb(153, 153, 211, 0.5)";
+    }
+
+    return {
+        currentPlayer
+    }
+
+
+})();
