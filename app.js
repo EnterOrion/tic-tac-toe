7e63089d4c8c945e9723e1e08a819b53d9899957
EnterOrion <orionwesson@gmail.com>
2022-12-18 15:44:56 +0100

    Declare winner function

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const gameBoard = (function() {
     let _board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    let winner;
+    let winningBoard = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+    ]
     let boardSquares = document.getElementsByClassName("square");
     document.getElementById("player1").style.backgroundColor = "rgb(153, 153, 211, 0.5)";
     for(let i=0; i<boardSquares.length; i++) {
@@ -14,6 +25,30 @@ const gameBoard = (function() {
                     document.getElementById("player2").style.backgroundColor = "beige"
                 }
                 _board[i] = gameController.currentPlayer.marker;
+                for (n=0; n<8; n++) {
+                    let x = 0;
+                    let o = 0;
+                    for(j=0; j<3; j++) {
+                        let a = Number(winningBoard[n][j]);
+                        if (_board[a] == "X")
+                            {
+                                x++;
+                                if (x == 3) {
+                                    winner = "Player 1";
+                                    document.getElementById("header").innerHTML = `${winner} wins!`;
+                                }
+                            }
+                        if (_board[a] == "O")
+                            {
+                                o++;
+                                if (o == 3) {
+                                    winner = "Player 2";
+                                    document.getElementById("header").innerHTML = `${winner} wins!`;
+                                }
+                            }
+                        
+                    }
+                }
                 
                 
             }
@@ -45,11 +80,9 @@ const gameController = (() => {
     function nextPlayer() {
         if (this.currentPlayer == player1) {
             this.currentPlayer = player2;
-            console.log(currentPlayer);
         }
         else {
             this.currentPlayer = player1;
-            console.log(currentPlayer)
         }
     }
 
